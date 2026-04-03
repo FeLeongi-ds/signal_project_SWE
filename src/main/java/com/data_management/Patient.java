9030173789ba841c4a05b36cc830a9b54da3e991
FeLeongi-ds <fede.leongi@gmail.com>
2026-04-03 12:41:02 +0200

    Fix complitation errors

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -49,9 +49,15 @@ public class Patient {
      *                  epoch
      * @param endTime   the end of the time range, in milliseconds since UNIX epoch
      * @return a list of PatientRecord objects that fall within the specified time
-     *         range
+     * range
      */
     public List<PatientRecord> getRecords(long startTime, long endTime) {
-        // TODO Implement and test this method
+        List<PatientRecord> result = new ArrayList<>();
+        for (PatientRecord record : patientRecords) {
+            if (record.getTimestamp() >= startTime && record.getTimestamp() <= endTime) {
+                result.add(record);
+            }
+        }
+        return result;
     }
 }
